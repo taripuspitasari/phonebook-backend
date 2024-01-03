d195da677b34139c8ee33196e2ec0fe39f39623a
Tari Puspita Sari <taripuspitasari12@gmail.com>
2024-01-03 20:45:52 +0700

    adding morgan middleware

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 app.use(express.json());
+
+morgan.token("body", req => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
