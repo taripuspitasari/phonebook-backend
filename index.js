fc1c697546ff25e69c26d015b606c6d3b2c0f421
Tari Puspita Sari <taripuspitasari12@gmail.com>
2024-01-03 15:01:25 +0700

    Implementing REST functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
@@ -28,8 +29,58 @@ app.get("/", (request, response) => {
   response.send("<h1>Hello anak manies</h1>");
 });
 
+app.get("/info", (request, response) => {
+  const people = persons.length;
+  const date = new Date();
+  response.send(
+    `<p>Phonebook has info for ${people} people</p> <br/> <p>${date}</p>`
+  );
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+  response.status(204).end();
+});
+
+const generatedId = () => {
+  return Math.floor(Math.random() * 100);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  } else if (persons.findIndex(person => person.name === body.name) !== -1) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generatedId(),
+  };
+
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
