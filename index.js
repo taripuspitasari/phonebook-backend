4959f80a6158672d6b6e86433232d3730f93d1f3
Tari Puspita Sari <taripuspitasari12@gmail.com>
2024-01-04 07:52:43 +0700

    modifying port variable

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const morgan = require("morgan");
+
 app.use(express.json());
+app.use(cors());
 
 morgan.token("body", req => JSON.stringify(req.body));
 app.use(
@@ -89,7 +92,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
